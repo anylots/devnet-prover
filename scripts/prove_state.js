ac627a3ed2aaf3bd8ad4d774cd619d0b91233566
anylots <842567494@qq.com>
2023-12-06 14:45:35 +0800

    querySetting

--- scripts/prove_state.js
+++ scripts/prove_state.js
@@ -74,11 +74,11 @@ async function querySetting(){
     console.log("committedBatch: " + committedBatch);
 
 
-    const pi = ethers.utils.hexlify(fs.readFileSync("../prover/proof/batch_28/pi_batch_agg.data"));
+    const pi = ethers.utils.hexlify(fs.readFileSync("../prover/proof/batch_1/pi_batch_agg.data"));
     console.log("pi: " + pi);
 
-    const pi_value = ethers.utils.hexStripZeros(fs.readFileSync("../prover/proof/batch_28/pi_batch_agg.data"));
-    console.log("pi_value: " + pi_value);
+    // const pi_value = ethers.utils.hexZeroPad(fs.readFileSync("../prover/proof/batch_1/pi_batch_agg.data"));
+    // console.log("pi_value: " + pi_value);
 
 
 }
